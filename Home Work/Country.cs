1d1ad9050ecc16a8f53161b3bd6da92ff0a51aca
Albert-Asadov <alberma@code.edu.az>
2022-12-11 17:26:56 +0400

    Home Task

---
+++ Home Work/Country.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Home_Work
+{
+    internal class Country
+    {
+       public string Name;
+
+        public static int count;
+
+       public  List<City> allCity = new List<City>();
+
+        static Country()
+        {
+            count = 0;
+        }
+
+
+        public Country(string name)
+        {
+            Name = name;            
+        }
+
+
+        public int TotalPopulation()
+        {
+            foreach (var x in allCity)
+            {
+               count += x.Population;
+
+            }
+            Console.WriteLine($"{Name}");
+            return count;
+          
+        }
+
+        public void FindAllByPopulation(int min, int max)
+        {
+            List<City> newCity = new List<City>();
+
+            foreach (var x in allCity)
+            {
+                if (x.Population > min  && x.Population < max)
+                {
+                    newCity.Add(x);
+
+                    Console.WriteLine($"{x.Name} {x.Population}");
+                }
+
+            }
+
+        }
+
+      
+    }
+}
---
+++ Home Work/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Home_Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Country country = new Country("Azerbaycan");
+
+            country.allCity.Add(new City("Baki", 4000000));
+            country.allCity.Add(new City("Qebele", 35000));
+            country.allCity.Add(new City("Shemkir", 80000));
+            country.allCity.Add(new City("Quba", 100000));
+            country.allCity.Add(new City("Gence", 2000000));
+            country.allCity.Add(new City("Sheki", 40000));
+
+
+            Console.WriteLine(country.TotalPopulation());
+
+            country.FindAllByPopulation(0, 4000000);
+            
+
+            
+
+
+            
+        }
+  
+    }
+}
